81c26716d4722bedeb24056fa68dd91980c0b733
Heba1488 <aaasd22009@gmail.com>
2025-09-03 03:47:45 +0300

    first commit

--- src/test/java/tests/C04GetAdjustSizeScreen.java
+++ src/test/java/tests/C04GetAdjustSizeScreen.java
@@ -1,5 +1,8 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class C04GetAdjustSizeScreen
 {
     // TC - 04
@@ -13,8 +16,13 @@ public static void main(String[] args) throws InterruptedException
     // TC - 04
 
 // Go to the w3school URL : https://www.w3schools.com/ 
+    WebDriver driver=new ChromeDriver();
+    driver.navigate().to("https://www.w3schools.com");
 // Print the position and size of the page. 
+    System.out.println(driver.manage().window().getPosition());
+    System.out.println(driver.manage().window().getSize());
 // Adjust the position and size of the page as desired. 
+
 // Test that the page is in the position and size you want. // Close the page.
 }
 }
