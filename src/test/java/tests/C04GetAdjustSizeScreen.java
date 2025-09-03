6edcde37901870e86f8df49331de5777bf01f6f3
Heba1488 <aaasd22009@gmail.com>
2025-09-03 04:25:57 +0300

    Heba

--- src/test/java/tests/C04GetAdjustSizeScreen.java
+++ src/test/java/tests/C04GetAdjustSizeScreen.java
@@ -1,5 +1,7 @@
 package tests;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -19,10 +21,22 @@ public static void main(String[] args) throws InterruptedException
     WebDriver driver=new ChromeDriver();
     driver.navigate().to("https://www.w3schools.com");
 // Print the position and size of the page. 
-    System.out.println(driver.manage().window().getPosition());
-    System.out.println(driver.manage().window().getSize());
-// Adjust the position and size of the page as desired. 
+    //**** Poisiotion******
+    Point position = driver.manage().window().getPosition();
+    System.out.println("position.getX() = " + position.getX());
+    System.out.println("position.getY() = " + position.getY());
+    System.out.println("position = " + position);
+    ///****size******
+    Dimension dimension = driver.manage().window().getSize();
+    System.out.println("dimension.getWidth() = " + dimension.getWidth());
+    System.out.println("dimension.getHeight() = " + dimension.getHeight());
+    System.out.println("dimension = " + dimension);
 
+// Adjust the position and size of the page as desired. 
+    driver.manage().window().setPosition(new org.openqa.selenium.Point(position.getX()+50, position.getY()-20));
+    driver.manage().window().setSize(new org.openqa.selenium.Dimension(600, 600));
 // Test that the page is in the position and size you want. // Close the page.
+    //driver.close();
+    driver.quit();
 }
 }
